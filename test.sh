3d4861e106b75bcfa032137b1badbc5da50d16ae
Sam Edwards <CFSworks@gmail.com>
2017-09-24 01:31:20 -0600

    travis: Move test invocation to its own script

---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+cd ${0%/*}
+
+# Establish working directory
+mkdir -p temp/
+cd temp/
+
+# Make sure Mikrotik CHR is downloaded
+if [ ! -e chr.vdi ]; then
+	wget -O chr.vdi https://download2.mikrotik.com/routeros/6.38.1/chr-6.38.1.vdi
+fi
+
+# Create snapshot image for testing
+if [ -f chr.qcow2 ]; then rm chr.qcow2; fi
+qemu-img create -f qcow2 -b chr.vdi chr.qcow2
+
+# Start RouterOS
+qemu-system-x86_64 -hda chr.qcow2 -nographic -device e1000,netdev=net0 \
+                   -netdev user,id=net0,net=192.168.254.0/24,dhcpstart=192.168.254.10,hostfwd=tcp:127.0.0.1:8728-:8728 > /dev/null &
+QEMU_PID=%%
+cleanup() {
+	kill $QEMU_PID
+}
+trap cleanup EXIT
+
+# Wait for it to become available
+echo Waiting for RouterOS to boot...
+python -c "while True:
+    try: __import__('librouteros').connect('127.0.0.1','admin','')
+    except: continue
+    else: break"
+echo ...DONE
+
+# Generate an inventory file
+cat > inventory <<EOF
+[routeros]
+localhost ansible_connection=local
+EOF
+
+# Fire off Ansible
+cd ..
+export ANSIBLE_LIBRARY=$PWD/library
+export PYTHONPATH=$ANSIBLE_LIBRARY
+ansible-playbook -i temp/inventory test/integration/placeholder.yml
