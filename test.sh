53b33f166482c9aa3dc0a68efbb32cabb3303cf9
Sam Edwards <CFSworks@gmail.com>
2017-10-04 19:28:00 -0600

    test: Fix port forwarding and inventory vars for SSH/CLI access

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ qemu-img create -f qcow2 -b chr.vdi chr.qcow2
 
 # Start RouterOS
 qemu-system-x86_64 -hda chr.qcow2 -nographic -device e1000,netdev=net0 \
-                   -netdev user,id=net0,net=192.168.254.0/24,dhcpstart=192.168.254.10,hostfwd=tcp:127.0.0.1:8728-:8728 > /dev/null &
+                   -netdev user,id=net0,net=192.168.254.0/24,dhcpstart=192.168.254.10,hostfwd=tcp:127.0.0.1:8728-:8728,hostfwd=tcp:127.0.0.1:22122-:22 > /dev/null &
 QEMU_PID=%%
 cleanup() {
 	kill $QEMU_PID
@@ -37,7 +37,7 @@ echo ...DONE
 cat > inventory <<EOF
 [routeros:vars]
 api='{ "host": "{{ inventory_hostname }}", "username": "admin", "password": "", "transport": "api" }'
-cli='{ "host": "{{ inventory_hostname }}", "username": "admin", "password": "", "transport": "cli" }'
+cli='{ "host": "{{ inventory_hostname }}", "port": 22122, "username": "admin", "password": "", "transport": "cli" }'
 
 [routeros]
 localhost ansible_connection=local ansible_python_interpreter=$(which python)
